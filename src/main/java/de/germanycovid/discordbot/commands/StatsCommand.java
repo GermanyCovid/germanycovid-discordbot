ab894e9168e8f5e5e282472a0c3e6731bb4c6e0e
Kacper Mura <kacper.mura@web.de>
2021-04-27 20:13:56 +0200

    Added stats command

---
+++ src/main/java/de/germanycovid/discordbot/commands/StatsCommand.java
@@ -0,0 +1,45 @@
+package de.germanycovid.discordbot.commands;
+
+import de.germanycovid.discordbot.DiscordBot;
+import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.utils.AttachmentOption;
+
+/**
+ *
+ * @author kacpe
+ */
+public class StatsCommand {
+    
+    private final DiscordBot discord;
+
+    public StatsCommand(DiscordBot discord) {
+        this.discord = discord;
+    }
+    
+    public void execute(GuildMessageReceivedEvent event) {
+        Message message = event.getMessage();
+        String[] args = message.getContentRaw().split(" ");
+        
+        InputStream inputStream;
+        try {
+            URLConnection url = new URL("https://api.germanycovid.de/images/germany").openConnection();
+            url.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+            inputStream = url.getInputStream();
+            event.getChannel().sendFile(inputStream, "stats.png", new AttachmentOption[0]).queue();
+        } catch (IOException ex) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setColor(new Color(235, 52, 94));
+            embed.setDescription("Leider konnte deine Anfrage nicht bearbeitet werden. Sollte es an uns liegen, so schreibe uns bitte eine E-Mail (support@germanycovid.de).");
+            this.discord.getBackendManager().sendMessage(event, embed.build());
+            this.discord.consoleError("The image for the statistics could not be loaded.");
+        }
+    }
+    
+}
--- src/main/java/de/germanycovid/discordbot/handlers/EventHandler.java
+++ src/main/java/de/germanycovid/discordbot/handlers/EventHandler.java
@@ -1,6 +1,10 @@
 package de.germanycovid.discordbot.handlers;
 
 import de.germanycovid.discordbot.DiscordBot;
+import de.germanycovid.discordbot.commands.InfoCommand;
+import de.germanycovid.discordbot.commands.StatsCommand;
+import java.text.MessageFormat;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -15,9 +19,13 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class EventHandler extends ListenerAdapter {
     
     private final DiscordBot discordBot;
-
+    private InfoCommand infoCommand;
+    private StatsCommand statsCommand;
+    
     public EventHandler(DiscordBot discordBot) {
         this.discordBot = discordBot;
+        this.infoCommand = new InfoCommand(discordBot);
+        this.statsCommand = new StatsCommand(discordBot);
     }
 
     @Override
@@ -27,7 +35,27 @@ public class EventHandler extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-
+        Message message = event.getMessage();
+        String prefix = this.discordBot.getBackendManager().getPrefix(event.getGuild());
+        
+        if (message.getMentionedUsers().stream().filter(t -> t.getId().equals(event.getGuild().getSelfMember().getId())).findFirst().orElse(null) != null) {
+            // HELP
+            return;
+        }
+        
+        if (!message.getContentRaw().toLowerCase().startsWith(prefix)) {
+            return;
+        }
+        
+        if (message.getContentRaw().toLowerCase().startsWith(prefix + "info")) {
+            infoCommand.execute(event);
+        } else if (message.getContentRaw().toLowerCase().startsWith(prefix + "stats")) {
+            statsCommand.execute(event);
+        } else {
+            return;
+        }
+        
+        this.discordBot.consoleInfo(MessageFormat.format("[SHARD {0}] {1} ({2}) ran command {3} in {4} (#{5})", event.getJDA().getShardInfo().getShardId(), event.getAuthor().getName(), event.getAuthor().getId(), message.getContentRaw().toLowerCase().split(" ")[0], event.getGuild().getName(), event.getChannel().getName()));
     }
     
 }
