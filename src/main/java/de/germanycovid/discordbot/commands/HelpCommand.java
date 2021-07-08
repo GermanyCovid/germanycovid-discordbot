eb9829331e0fa7f2793d6f0a67b52d71e3787444
Kacper Mura <kacper.mura@web.de>
2021-07-09 00:09:50 +0200

    Update HelpCommand.java

--- src/main/java/de/germanycovid/discordbot/commands/HelpCommand.java
+++ src/main/java/de/germanycovid/discordbot/commands/HelpCommand.java
@@ -27,13 +27,14 @@ public class HelpCommand {
         embed.setDescription("GermanyCovid – Wir bieten dir täglich mehrere Daten kurzgefasst auf einen Blick an, um Dich mit den neusten Daten vertraut zu machen. Neben den Neuinfektionen und den Todesfällen bieten wir auch die Zahlen zu den Intensivstationen, sowie zu den jeweiligen Bundesländern und Landkreisen an.\n\n** **");
         embed.addField("Befehle", "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "info** - Hier findest Du Fakten und Informationen zum Bot.\n"
                 + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "stats** - Siehe die wichtigsten Daten für die Bundesrepublik.\n"
-                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "states** - Finde die Daten für die Bundesländer\n"
-                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "districts** - Finde die Daten für die 412 Landkreise bzw. Kreisfreien Städte\n"
+                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "states** - Finde die Daten für die Bundesländer.\n"
+                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "districts** - Finde die Daten für die 412 Landkreise bzw. Kreisfreien Städte.\n"
+                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "vaccinations** - Finde die neusten Daten bezüglich der Herdenimmunität.\n"
                 + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "hospital** - Finde die wichtigsten Daten von den Intensivstationen.\n"
                 + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "support** - Du benötigst Hilfe? Hier findest du unsere Supportwege.", false);
         if(this.discord.getBackendManager().checkForPermissions(event.getMember())) {
-            embed.addField("Admin Befehle", "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "prefix <prefix>** - Änder den Prefix\n"
-                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "channel <#channel>** - Gebe einen Channel an, wo nur noch Commands ausführbar sein sollen", false);
+            embed.addField("Admin Befehle", "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "prefix <prefix>** - Änder den Prefix.\n"
+                + "**" + this.discord.getBackendManager().getPrefix(event.getGuild()) + "channel <#channel>** - Gebe einen Channel an, wo nur noch Commands ausführbar sein sollen.", false);
         }
         this.discord.getBackendManager().sendMessage(event, embed.build());
     }
